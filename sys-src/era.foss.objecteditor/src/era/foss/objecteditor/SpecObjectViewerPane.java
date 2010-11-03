6780e466d7dfb48ecedeaf77d4febf7ac84fc8c5
cpnatwork <cpnatwork@googlemail.com>
2010-11-03 22:12:32 +0000

    * contributor

--- sys-src/era.foss.objecteditor/src/era/foss/objecteditor/SpecObjectViewerPane.java
+++ sys-src/era.foss.objecteditor/src/era/foss/objecteditor/SpecObjectViewerPane.java
@@ -1,3 +1,21 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2010 'Three Good Friends' (3GF).
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *   Georg Blaschke 
+ *   Christoph P. Neumann  
+ *   Bernd Haberstumpf
+ *
+ * </copyright>
+ *
+ * $Id$
+ */
 package era.foss.objecteditor;
 
 import org.eclipse.emf.common.ui.ViewerPane;
