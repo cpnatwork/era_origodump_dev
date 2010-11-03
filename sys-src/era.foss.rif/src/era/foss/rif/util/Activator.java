7323910383b485ca91ac633950de12df2ea50040
cpnatwork <cpnatwork@googlemail.com>
2010-11-03 19:43:32 +0000

    * remove copy-paste generated-annotations from activator

--- sys-src/era.foss.rif/src/era/foss/rif/util/Activator.java
+++ sys-src/era.foss.rif/src/era/foss/rif/util/Activator.java
@@ -10,32 +10,20 @@ import org.eclipse.emf.common.util.ResourceLocator;
 
 /**
  * This is the central singleton for the Rif edit plugin.
- * <!-- begin-user-doc -->
- * <!-- end-user-doc -->
- * @generated
  */
 public final class Activator extends EMFPlugin {
     /**
      * Keep track of the singleton.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
      */
     public static final Activator INSTANCE = new Activator();
 
     /**
      * Keep track of the singleton.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
      */
     private static Implementation plugin;
 
     /**
      * Create the instance.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
      */
     public Activator() {
         super( new ResourceLocator[]{} );
@@ -43,10 +31,7 @@ public final class Activator extends EMFPlugin {
 
     /**
      * Returns the singleton instance of the Eclipse plugin.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
      * @return the singleton instance.
-     * @generated
      */
     @Override
     public ResourceLocator getPluginResourceLocator() {
@@ -55,10 +40,7 @@ public final class Activator extends EMFPlugin {
 
     /**
      * Returns the singleton instance of the Eclipse plugin.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
      * @return the singleton instance.
-     * @generated
      */
     public static Implementation getPlugin() {
         return plugin;
@@ -66,16 +48,10 @@ public final class Activator extends EMFPlugin {
 
     /**
      * The actual implementation of the Eclipse <b>Plugin</b>.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
      */
     public static class Implementation extends EclipsePlugin {
         /**
          * Creates an instance.
-         * <!-- begin-user-doc -->
-         * <!-- end-user-doc -->
-         * @generated
          */
         public Implementation() {
             super();
