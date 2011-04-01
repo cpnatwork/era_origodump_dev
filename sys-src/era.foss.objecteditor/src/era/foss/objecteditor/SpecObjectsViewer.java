fd20f6dbb2faa2b5536b800b794bdb6456efda68
cpnatwork <cpnatwork@googlemail.com>
2011-04-01 17:45:50 +0000

    * Java File Header (Copyright & License)

--- sys-src/era.foss.objecteditor/src/era/foss/objecteditor/SpecObjectsViewer.java
+++ sys-src/era.foss.objecteditor/src/era/foss/objecteditor/SpecObjectsViewer.java
@@ -1,21 +1,21 @@
-/**
- * <copyright>
- *
- * Copyright (c) 2010 'Three Good Friends' (3GF).
- * All rights reserved.   This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: 
- *   Georg Blaschke 
- *   Christoph P. Neumann  
- *   Bernd Haberstumpf
- *
- * </copyright>
- *
- * $Id$
- */
+/**************************************************************************
+* ERA - Eclipse Requirements Analysis
+* ==============================================
+* Copyright (C) 2009-2011 by Georg Blaschke, Christoph P. Neumann
+* and Bernd Haberstumpf (http://era.origo.ethz.ch)
+**************************************************************************
+* Licensed under the Eclipse Public License - v 1.0 (the "License");
+* you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+* http://www.eclipse.org/org/documents/epl-v10.html
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**************************************************************************
+* $Id$
+*************************************************************************/
 package era.foss.objecteditor;
 
 import org.eclipse.emf.common.command.BasicCommandStack;
