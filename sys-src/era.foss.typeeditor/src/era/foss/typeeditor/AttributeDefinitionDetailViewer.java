4bdec9ca8172859248cf8d6115e3840e86c9a194
schorsch <schorsch@franken-online.de>
2011-06-13 17:33:11 +0000

    + Provide selection of default values for Enumerations

--- sys-src/era.foss.typeeditor/src/era/foss/typeeditor/AttributeDefinitionDetailViewer.java
+++ sys-src/era.foss.typeeditor/src/era/foss/typeeditor/AttributeDefinitionDetailViewer.java
@@ -36,7 +36,10 @@ import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.jface.databinding.swt.SWTObservables;
 import org.eclipse.jface.databinding.viewers.ObservableListContentProvider;
 import org.eclipse.jface.databinding.viewers.ObservableMapCellLabelProvider;
+import org.eclipse.jface.viewers.CheckStateChangedEvent;
 import org.eclipse.jface.viewers.CheckboxTableViewer;
+import org.eclipse.jface.viewers.ICheckStateListener;
+import org.eclipse.jface.viewers.ICheckStateProvider;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
@@ -51,6 +54,8 @@ import era.foss.erf.AttributeDefinition;
 import era.foss.erf.AttributeDefinitionEnumeration;
 import era.foss.erf.AttributeDefinitionSimple;
 import era.foss.erf.AttributeValue;
+import era.foss.erf.AttributeValueEnumeration;
+import era.foss.erf.EnumValue;
 import era.foss.erf.ErfPackage;
 import era.foss.erf.impl.ErfFactoryImpl;
 import era.foss.typeeditor.ui.BindingCheckBox;
@@ -134,7 +139,7 @@ public class AttributeDefinitionDetailViewer extends AbstractDetailViewer {
      */
     private void createDetailsEnumeration() {
         assert (master.getValue() instanceof AttributeDefinitionEnumeration);
-        AttributeDefinitionEnumeration attributeDefinitionEnum = (AttributeDefinitionEnumeration)master.getValue();
+        final AttributeDefinitionEnumeration attributeDefinitionEnum = (AttributeDefinitionEnumeration)master.getValue();
 
         // create label for multiValued property
         Label label = new Label( detailComposite, SWT.NONE );
@@ -151,12 +156,12 @@ public class AttributeDefinitionDetailViewer extends AbstractDetailViewer {
         Button defaultValueCheckbox = createDefaultValueCheckbox( attributeDefinitionEnum,
                                                                   ErfPackage.Literals.ATTRIBUTE_DEFINITION_ENUMERATION__DEFAULT_VALUE );
 
-        // create table for selecting default enumeration values
+        // create table for selecting default EnumValue elements
         Table defaultValueTable = new Table( detailComposite, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION | SWT.CHECK );
         defaultValueTable.setLinesVisible( true );
         defaultValueTable.setHeaderVisible( true );
 
-        CheckboxTableViewer defaultValueTableViewer = new CheckboxTableViewer( defaultValueTable );
+        final CheckboxTableViewer defaultValueTableViewer = new CheckboxTableViewer( defaultValueTable );
         defaultValueTable.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, true, 2, 1 ) );
 
         final ObservableListContentProvider cp = new ObservableListContentProvider();
@@ -164,7 +169,6 @@ public class AttributeDefinitionDetailViewer extends AbstractDetailViewer {
 
         TableViewerColumn column = new TableViewerColumn( defaultValueTableViewer, SWT.NONE );
 
-        // columnLayout.setColumnData( column.getColumn(), new ColumnWeightData( 20, 20 ) );
         column.getColumn().setResizable( false );
         column.getColumn().setMoveable( false );
         column.getColumn().setText( Ui.getUiName( ErfPackage.Literals.ATTRIBUTE_VALUE_ENUMERATION__VALUES ) );
@@ -176,13 +180,54 @@ public class AttributeDefinitionDetailViewer extends AbstractDetailViewer {
         IObservableMap attributeMap = elementProperty.observeDetail( cp.getKnownElements() );
         column.setLabelProvider( new ObservableMapCellLabelProvider( attributeMap ) );
 
-        // observe
+        // Add provider for the check stare of the table viewer
+        // A value is checked if it is referred by the Default value of the AttributeDefintionEnumeration
+        defaultValueTableViewer.setCheckStateProvider( new ICheckStateProvider() {
+            @Override
+            public boolean isGrayed( Object element ) {
+                return false;
+            }
+
+            @Override
+            public boolean isChecked( Object element ) {
+                AttributeValueEnumeration enumDefaultValue = attributeDefinitionEnum.getDefaultValue();
+                return enumDefaultValue != null && enumDefaultValue.getValues().contains( element );
+            }
+        } );
+
+        // If a EnumValue is selected add it to the list of Default values for the AttributeDefintionEnumeration
+        // Otherwise remove it from the list of default values
+        defaultValueTableViewer.addCheckStateListener( new ICheckStateListener() {
+
+            @Override
+            public void checkStateChanged( CheckStateChangedEvent event ) {
+                // we don't need to check if the default value object is null as
+                // in this case table viewer is disabled
+                assert (attributeDefinitionEnum.getDefaultValue() != null);
+
+                EnumValue enumValue = ((EnumValue)event.getElement());
+                Command command = null;
+
+                if( event.getChecked() == true ) {
+                    command = AddCommand.create( editingDomain,
+                                                 attributeDefinitionEnum.getDefaultValue(),
+                                                 ErfPackage.Literals.ATTRIBUTE_VALUE_ENUMERATION__VALUES,
+                                                 enumValue );
+                } else {
+                    command = RemoveCommand.create( editingDomain,
+                                                    attributeDefinitionEnum.getDefaultValue(),
+                                                    ErfPackage.Literals.ATTRIBUTE_VALUE_ENUMERATION__VALUES,
+                                                    enumValue );
+                }
+                editingDomain.getCommandStack().execute( command );
+            }
+        } );
+
+        // observe values of DatatypeDefinition selected for the AttributeDefintionEnumeration
         IEMFListProperty enumerationProperty = EMFProperties.list( FeaturePath.fromList( new EStructuralFeature[]{
             ErfPackage.Literals.ATTRIBUTE_DEFINITION__TYPE,
             ErfPackage.Literals.DATATYPE_DEFINITION_ENUMERATION__SPECIFIED_VALUES} ) );
-
         IObservableList dataTypeEnumValueObserver = enumerationProperty.observe( attributeDefinitionEnum );
-
         defaultValueTableViewer.setInput( dataTypeEnumValueObserver );
 
         // only enable table viewer in case a default value exists
@@ -193,36 +238,36 @@ public class AttributeDefinitionDetailViewer extends AbstractDetailViewer {
 
     private Button createDefaultValueCheckbox( final AttributeDefinition attributeDefinition,
                                                final EReference defaultValueEReference ) {
-    
+
         AttributeValue defaultValue = (AttributeValue)attributeDefinition.eGet( defaultValueEReference );
-    
+
         // label for enabling default value
         Label label = new Label( detailComposite, SWT.NONE );
         label.setText( "Use Default value" );
         label.setLayoutData( new GridData( SWT.LEFT, SWT.CENTER, true, false ) );
-    
+
         // checkbox for enabling default value
         // when the checkbox is set a default value object will be created
         Button defaultValueCheckbox = new Button( detailComposite, SWT.CHECK );
         defaultValueCheckbox.setSelection( defaultValue != null );
         defaultValueCheckbox.addListener( SWT.Selection, new Listener() {
-    
+
             @Override
             public void handleEvent( Event event ) {
                 Button defaultValueCheckbox = (Button)event.widget;
-    
+
                 AttributeValue defaultValue = (AttributeValue)attributeDefinition.eGet( defaultValueEReference );
-    
+
                 if( defaultValueCheckbox.getSelection() == false ) {
                     if( defaultValue != null ) {
                         Command defaultValueRemoveCommand = RemoveCommand.create( editingDomain, defaultValue );;
                         editingDomain.getCommandStack().execute( defaultValueRemoveCommand );
                     }
-    
+
                 }
                 if( defaultValueCheckbox.getSelection() == true ) {
                     if( defaultValue == null ) {
-    
+
                         AttributeValue newDefaultValue = (AttributeValue)ErfFactoryImpl.eINSTANCE.create( defaultValueEReference.getEReferenceType() );
                         newDefaultValue.setDefinition( attributeDefinition );
                         Command defaultValueAddCommand = AddCommand.create( editingDomain,
