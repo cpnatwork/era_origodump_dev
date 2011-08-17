31d1e7a662a1c00ce415f4f0a68b8e09505568d8
cpnatwork <cpnatwork@googlemail.com>
2011-08-17 20:17:09 +0000

    * loadtest ERF corrected

--- sys-src/era.foss.testing/resources/loadtest_createBigChunk.cmd
+++ sys-src/era.foss.testing/resources/loadtest_createBigChunk.cmd
@@ -1,9 +1,9 @@
 @echo off
 
-FOR /L %%I IN (100000,1,200000) DO (
+FOR /L %%I IN (100000,1,150000) DO (
 echo INFO: %%I 1>&2
-echo     ^<specObjects iD="%%I_soID" type="//@coreContent/@specTypes.0"^>
-echo      ^<values xsi:type="era.foss.rif:AttributeValueSimple" iD="%%I_vID_a" theValue="5" definition="//@coreContent/@specTypes.0/@specAttributes.0"/^>
-echo      ^<values xsi:type="era.foss.rif:AttributeValueSimple" iD="%%I_vID_b" theValue="Hierarchy" definition="//@coreContent/@specTypes.0/@specAttributes.1"/^>
+echo    ^<specObjects iD="%%I_so" type="//@coreContent/@specTypes.0"^>
+echo      ^<values xsi:type="era.foss.erf:AttributeValueSimple" iD="%%I_vA" definition="//@coreContent/@specTypes.0/@specAttributes.0" theValue="%%I"/^>
+echo      ^<values xsi:type="era.foss.erf:AttributeValueSimple" iD="%%I_vB" definition="//@coreContent/@specTypes.0/@specAttributes.1" theValue="lol"/^>
 echo    ^</specObjects^>
 )
